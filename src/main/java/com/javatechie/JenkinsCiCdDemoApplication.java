20083eb3a0c4f4ff2e2c966ffe0474a6e9387866
djuric-ivan <djuricivan43@gmail.com>
2024-08-26 14:03:34 +0200

    Initial commit

---
+++ src/main/java/com/javatechie/JenkinsCiCdDemoApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCiCdDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCiCdDemoApplication.class, args);
+    }
+
+}
